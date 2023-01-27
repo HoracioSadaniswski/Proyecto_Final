3c1db6a2edaea666c2223c1cd3e0174ff3ce776d
Horacio Sadaniswski <rhs9208@gmail.com>
2023-01-27 17:43:50 -0300

    4.2 Finalización menú responsivo

--- miCv.js
+++ miCv.js
@@ -18,14 +18,13 @@ $(document).ready(function(){
 });
 
 /*  Responsive nav */
+
 $(document).ready(main);
 
 var contador = 1;
 
 function main(){
 	$('.menu_bar').click(function(){
-		$('nav').toggle(); 
-
 		if(contador == 1){
 			$('nav').animate({
 				left: '0'
